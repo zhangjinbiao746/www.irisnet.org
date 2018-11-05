a93467338f31e0ff1c422c786de8dfdc536c30f3
zhjibi742626083 <jinbiao@bianjie.ai>
2018-11-05 15:13:43 +0800

    #IRISHOME-71 Testnet页面改版

--- src/components/TestNet.vue
+++ src/components/TestNet.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="testnet_page">
         <div class="index" style="height:100%; z-index: 1" @click="closeMenu">
-            <headr></headr>
+            <topHeader></topHeader>
                 <div class="testnet_content">
                     <div class="testnet_title">
                         <p>{{title}}</p>
@@ -179,11 +179,11 @@
 
 <script>
     import foot from './foot';
-    import headr from './head';
+    import topHeader from './head';
     import message from '../common/message';
     export default {
         name: "terms",
-        components: {foot,headr},
+        components: {foot,topHeader},
         data(){
             return {
                 title:'',
