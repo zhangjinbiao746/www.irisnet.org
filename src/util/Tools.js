6ff3cabda368bd957bac3b46cd6ea84411ec6ca4
zhjibi742626083 <jinbiao@bianjie.ai>
2019-01-15 19:50:05 +0800

    #IRISHOME-96 更新IRIS Network简介轮播图修改中英文文案

--- src/util/Tools.js
+++ src/util/Tools.js
@@ -1,10 +1,10 @@
 export default class Tools {
 	static currentDeviceIsPersonComputer(){
 		const userAgent = navigator.userAgent;
-		const Agents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
+		const agents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
 		let flag = true;
-		for (let i = 0; i < Agents.length; i++) {
-			if (userAgent.includes(Agents[i]) > 0) {
+		for (let i = 0; i < agents.length; i++) {
+			if (userAgent.includes(agents[i]) > 0) {
 				flag = false;
 				break;
 			}
