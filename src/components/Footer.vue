3798b2c423e4a1612cb65e1a71b0a01831430ec0
zhjibi742626083 <jinbiao@bianjie.ai>
2019-02-25 18:44:32 +0800

    #IRISHOME-121

---
+++ src/components/Footer.vue
@@ -0,0 +1,95 @@
+<template>
+    <div class="footer_container">
+        <div class="footer_wrap">
+            <div class="content">
+                <div class="community_link_content">
+                    <div class="social_community_content">
+                        <p class="community">Social Community</p>
+                        <ul class="community_link_list">
+                            <li class="community_link_item" v-for="item in $store.state.messages.footer.socialCommunity" :key="item.id">
+                                <a :href="item.href" target="_blank">
+                                    <img :src='UrlSrc + item.src'>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="tech_community_content">
+                        <p class="community">Tech Community</p>
+                        <ul class="community_link_list">
+                            <li class="community_link_item" v-for="item in $store.state.messages.footer.techCommunity" :key="item.id">
+                                <a :href="item.href" target="_blank">
+                                    <img :src="UrlSrc + item.src">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="newsletter_content">
+                    <h4 class="newsletter_title">{{$store.state.messages.footer.submitBtn.title}}</h4>
+                    <input v-model="mailAddress" class="email_input" :class="flShowError ? 'error_style' : ' '" type="text" :placeholder="$store.state.messages.placehooder.placehooder">
+                    <p :class="flShowError ? 'show_error' : 'hide_error'">{{$store.state.messages.errEmail.err}}</p>
+                    <button class="submit_btn" @click="commitMaile">{{subscription}}</button>
+                </div>
+            </div>
+        </div>
+        <div class="copyright_wrap">
+            <div class="copyright_content">
+                <p class="irisnet">{{$store.state.messages.footer.irisnetInproduction}}</p>
+                <p><span class="copyright">Copyright &#169 2018 IRIS Foundation Ltd. All rights reserved.</span><a :href="'/privacy/?lang='+$store.state.lang"> Privacy</a> <span class="connector"> & </span> <a :href="'/terms?lang='+$store.state.lang">Terms</a></p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    export default {
+        name: "Footer",
+        data () {
+            return {
+                mailAddress: '',
+                flShowError: false,
+                submitTimer: '',
+                errorTimer: '',
+                subscription: this.$store.state.messages.footer.submitBtn.subscribe
+            }
+        },
+        methods:{
+            commitMaile(){
+                clearTimeout(this.submitTimer);
+                clearTimeout(this.errorTimer);
+                let address =  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                if(address.exec(this.mailAddress)){
+                    this.flShowError = false;
+                }else {
+                    this.flShowError = true;
+                    let that = this;
+                    this.errorTimer = setTimeout(function () {
+                        that.flShowError = false;
+                    },2000)
+                    return
+                }
+                axios({
+                    method: 'post',
+                    url:"/",
+                    data: {
+                        email:this.mailAddress,
+                    }
+                }).then((data)=>{
+                    this.subscription = this.$store.state.messages.submit.success;
+                    let that = this;
+                    this.submitTimer = setTimeout(function () {
+                        that.subscription = that.$store.state.messages.submit.Subscribe;
+                        that.mailAddress = "";
+                    },2000)
+                })
+                .catch((e)=>{
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped lang="less">
+    @import "../assets/style/footer";
+</style>
