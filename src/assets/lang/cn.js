c2b1c0ae4e05c6e4e27964235d4ab6730b20a864
zjb <zhangjinbiao9693@gmail.com>
2019-07-27 20:03:37 +0800

    add mintscan explorer

--- src/assets/lang/cn.js
+++ src/assets/lang/cn.js
@@ -741,6 +741,10 @@ export const message = {
                         {
                             itemName: 'Hubble',
                             href: 'https://hubble.figment.network/iris/chains/irishub'
+                        },
+                        {
+                            itemName: 'Mintscan',
+                            href: 'https://irishub.mintscan.io/'
                         }
                     ],
                     content:'您可通过以上区块链浏览器查询和分析 IRIS Hub 的区块、交易、验证人等信息。',
