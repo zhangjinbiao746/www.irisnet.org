d3631b42ad2db98d37f9694a43ab06f64c96566e
zhjibi742626083 <jinbiao@bianjie.ai>
2019-09-06 15:53:48 +0800

    add github link in press-kit page

--- src/assets/lang/en.js
+++ src/assets/lang/en.js
@@ -573,16 +573,14 @@ export const message = {
                 }
             ],
             irisService:{
+                leftTitle:"About ISCH",
                 title:'IRIS Service Consortium Hub (ISCH)',
-                content:'ISCH is the consortium interchain service Hub. It is a blockchain developed using IRISnet iService infrastructure. Through iService, it can efficiently support interoperability among heterogenous systems, including trustworthy on chain off chain coordination. ISCH supports distributed application systems that participate in business collaboration as service consumer to efficiently call data and computing services published on ISCH by other blockchains (even traditional systems) via iService. As a consortium blockchain, it also includes modules to provide id management, storage as well as auditing services etc.',
                 href:'https://github.com/irisnet/irisnet/blob/master/pressKit.md#iris-service-consortium-hub-isch',
-                readMoreBtn: 'Read More'
             },
             beanContent:{
+                leftTitle:"About BEAN",
                 title: 'BEAN: Blockchain Edge Analytics Network',
-                content: 'BEAN is a distributed data analytics and exchange permissioned blockchain network, developed with the iServices framework from IRISnet. It supports privacy preserving data analysis, respects the rights of data owners, and allows auditing of data usage (more details on its business case can be found at IRISnet whitepaper). Furthermore, BEAN also enables medical data to be analyzed and exchanged freely and securely with benefits provided blockchain technology. This allows institutions, like insurance companies, hospitals as well as pharmaceutical companies, to collaborate to provide better services for patients.',
                 href:'https://github.com/irisnet/irisnet/blob/master/pressKit.md#bean-blockchain-edge-analytics-network',
-                readMoreBtn: 'Read More'
             }
         },
         title: 'Community',
