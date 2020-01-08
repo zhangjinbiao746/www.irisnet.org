0024a15c83ec6014ef20015a503ef44ff71b4519
zhjibi742626083 <jinbiao@bianjie.ai>
2020-01-08 11:43:33 +0800

    update Staking Tools

--- server.js
+++ server.js
@@ -250,7 +250,7 @@ app.get('*', isProd ? render : (req, res) => {
 })
 
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 30323
 app.listen(port, () => {
   console.log(`server started at localhost:${port}`)
 })
