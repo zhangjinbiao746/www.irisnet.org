7f3025742ce2592ca3b10cc3b362538a75f6e5fc
jinbiao-bianjie <zhangjinbiao9693@gmail.com>
2020-02-28 21:04:58 +0800

    adapt the mobile style

--- src/components/NavigationThree.vue
+++ src/components/NavigationThree.vue
@@ -68,7 +68,7 @@
                 </ul>
             </div>
             <div class="navigation_right_content">
-                <div class="mobile_right_content">
+                <div class="mobile_right_content" @click="showMobileMenu()">
                     <img src="../assets/irisnetThree/mobile_menu_icon.png" alt="">
                 </div>
                 <div class="navigation_right_link_content">
@@ -86,6 +86,73 @@
                 </div>
             </div>
         </div>
+        <div class="navigation_mobile_content" v-show="$store.state.showMobileMenu">
+            <ul class="navigation_mobile_list_content">
+                <li class="navigation_mobile_list" @click="showAboutUs()">
+                    <div class="navigation_mobile_item">{{$t("message.navigation.aboutUs")}}</div>
+                    <ul class="navigation_mobile_second_menu" v-show="flShowAboutUs">
+                        <li class="navigation_mobile_second_list_item" >
+                            <router-link :to="`/mainnet`">{{$t("message.navigation.secondMenu.mainnet")}}</router-link>
+                        </li>
+                        <li class="navigation_mobile_second_list_item">
+                            <a v-if="$i18n.locale === 'EN'" href="https://github.com/irisnet/irisnet/blob/master/WHITEPAPER.md" target="_blank">{{$t("message.navigation.secondMenu.whitepaper")}}</a>
+                            <a v-if="$i18n.locale === 'CN'" href="https://github.com/irisnet/irisnet/blob/master/白皮书.md" target="_blank">{{$t("message.navigation.secondMenu.whitepaper")}}</a>
+                        </li>
+                        <li class="navigation_mobile_second_list_item">
+                            <router-link :to="`/announcements`">{{$t("message.navigation.secondMenu.announcement")}}</router-link>
+                        </li>
+                        <li class="navigation_mobile_second_list_item">
+                            <router-link :to="`/collaboration`">{{$t("message.navigation.secondMenu.collaboration")}}</router-link>
+                        </li>
+                    </ul>
+                </li>
+                <li class="navigation_mobile_list" @click="showDeveloper()">
+                    <div class="navigation_mobile_item">{{$t("message.navigation.developer")}}</div>
+                    <ul class="navigation_mobile_second_menu" v-show="flShowDeveloper">
+                        <li class="navigation_mobile_second_list_item">
+                            <router-link :to="`/devportal`">{{$t("message.navigation.secondMenu.devPortal")}}</router-link>
+                        </li>
+                        <li class="navigation_mobile_second_list_item">
+                            <router-link :to="`/testnets`">{{$t("message.navigation.secondMenu.testnet")}}</router-link>
+                        </li>
+                        <li class="navigation_mobile_second_list_item">
+                            <a v-if="$i18n.locale === 'EN'" href="https://www.irisnet.org/docs/" target="_blank">{{$t("message.navigation.secondMenu.doc")}}</a>
+                            <a v-if="$i18n.locale === 'CN'" href="https://www.irisnet.org/docs/zh/" target="_blank">{{$t("message.navigation.secondMenu.doc")}}</a>
+                        </li>
+                        <li class="navigation_mobile_second_list_item">
+                            <a href="https://cosmos.network/ibc" target="_blank">{{$t("message.navigation.secondMenu.ibc")}}</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="navigation_mobile_list" @click="showEcosystem()">
+                    <div class="navigation_mobile_item">{{$t("message.navigation.community")}}</div>
+                    <ul class="navigation_mobile_second_menu" v-show="flShowEcosystem">
+                        <li class="navigation_mobile_second_list_item">
+                            <router-link :to="`/community/press-kit`">{{$t("message.navigation.secondMenu.pressKit")}}</router-link>
+                        </li>
+                        <li class="navigation_mobile_second_list_item">
+
+                            <router-link :to="`/irisnet-bianjie`">{{$t("message.navigation.secondMenu.irisnetBianjie")}}</router-link>
+                        </li>
+                        <li class="navigation_mobile_second_list_item">
+                            <router-link :to="`/ecosystem`">{{$t("message.navigation.secondMenu.IRISEcosystem")}}</router-link>
+                        </li>
+                        <li class="navigation_second_list_item">
+                            <router-link :to="`/events`">{{$t("message.navigation.secondMenu.events")}}</router-link>
+                        </li>
+                        <li class="navigation_mobile_second_list_item">
+                            <router-link :to="`/community`">{{$t("message.navigation.secondMenu.community")}}</router-link>
+                        </li>
+                    </ul>
+                </li>
+                <li class="navigation_mobile_list">
+                    <a href="https://forum.irisnet.org/" target="_blank">{{$t("message.navigation.forum")}}</a>
+                </li>
+                <li class="navigation_mobile_list">
+                    <a href="https://medium.com/irisnet-blog" target="_blank">{{$t("message.navigation.blog")}}</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -94,8 +161,16 @@
         name: "NavigationThree",
         data(){
             return {
-
+                flShowMobileMenu:false,
+                flShowAboutUs: false,
+                flShowDeveloper : false,
+                flShowEcosystem: false,
             }
+        },
+        watch:{
+            $router(){
+                console.log("?????")
+            },
         },
         methods:{
             changeLang(){
@@ -104,6 +179,24 @@
                 }else {
                     this.$i18n.locale = 'CN'
                 }
+            },
+            showMobileMenu(){
+                this.$store.commit('showMobileMenu',!this.$store.state.showMobileMenu)
+            },
+            showAboutUs(){
+                this.flShowAboutUs = !this.flShowAboutUs;
+                this.flShowDeveloper = false;
+                this.flShowEcosystem = false;
+            },
+            showDeveloper(){
+                this.flShowDeveloper = !this.flShowDeveloper;
+                this.flShowAboutUs = false;
+                this.flShowEcosystem = false;
+            },
+            showEcosystem(){
+                this.flShowEcosystem = !this.flShowEcosystem;
+                this.flShowAboutUs = false;
+                this.flShowDeveloper = false;
             }
         }
     }
@@ -115,6 +208,9 @@
         background: #111370;
         position: fixed;
         z-index: 1000;
+        .navigation_mobile_content{
+            display: none;
+        }
         .navigation_content_wrap{
             max-width: 12rem;
             margin: 0 auto;
@@ -140,6 +236,7 @@
                         height: 0.8rem;
                         line-height: 0.8rem;
                         text-align: center;
+                        cursor: pointer;
                         .navigation_second_menu{
                             width: 1.6rem;
                             text-align: left;
@@ -208,17 +305,20 @@
                     align-items: center;
                     margin-left: 0.4rem;
                     .navigation_lang_content{
+                        width: 1.5rem;
                         border: 0.02rem solid #5E82F1;
-                        padding: 0.1rem 0.18rem 0.1rem 0.33rem;
+                        padding: 0.1rem 0;
                         border-radius: 0.2rem;
                         color: #fff;
+                        cursor: pointer;
                         span{
-                            padding-right: 0.3rem;
+                            width: 0.55rem;
+                            display: inline-block;
+                            padding: 0 0.3rem;
                         }
                         i{
                             display: inline-block;
                             width: 0.12rem;
-                            padding-rights: 0.3rem;
                             img{
                                 width: 100%;
                             }
@@ -228,7 +328,7 @@
             }
         }
     }
-    @media screen  and (max-width: 930px){
+    @media screen  and (max-width: 955px){
         .navigation_container{
             .navigation_content_wrap{
                 padding: 0.2rem 0.4rem;
@@ -242,11 +342,156 @@
                         display: none;
                     }
                     .mobile_right_content{
-                        .mobile_right_content{
-                            width: 0.3rem;
-                            display: none;
+                        display: block;
+                        width: 0.3rem;
+                        img{
+                            width: 100%;
+                        }
+                    }
+                }
+            }
+            .navigation_mobile_content{
+                display: block;
+                padding-bottom: 0.02rem;
+                .navigation_mobile_list_content{
+                    text-align: center;
+                    .navigation_mobile_list{
+                        padding: 0.1rem 0;
+                        a{
+                            color: #fff;
+                        }
+                        .navigation_mobile_item{
+                            font-size: 0.16rem;
+                            font-weight:400;
+                            color:rgba(255,255,255,1);
+                            line-height: 0.19rem;
+                        }
+                        .navigation_mobile_second_menu{
+                            background: #030566;
+                            margin-top: 0.2rem;
+                            .navigation_mobile_second_list_item {
+                                padding: 0.15rem 0px;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    @media screen  and (max-width: 768px){
+        .navigation_container{
+            .navigation_content_wrap{
+                padding: 0.2rem;
+                .navigation_left_content{
+                    .navigation_left_list_content{
+                        display: none;
+                    }
+                }
+                .navigation_right_content{
+                    .navigation_right_lang_content{
+                        margin-left: 0.2rem;
+                    }
+                    .navigation_right_link_content{
+                        display: none;
+                    }
+                    .mobile_right_content{
+                        display: block;
+                        width: 0.3rem;
+                        img{
+                            width: 100%;
+                        }
+                    }
+                }
+            }
+            .navigation_mobile_content{
+                display: block;
+                padding-bottom: 0.02rem;
+                .navigation_mobile_list_content{
+                    text-align: center;
+                    .navigation_mobile_list{
+                        padding: 0.1rem 0;
+                        a{
+                            color: #fff;
+                        }
+                        .navigation_mobile_item{
+                            font-size: 0.16rem;
+                            font-weight:400;
+                            color:rgba(255,255,255,1);
+                            line-height: 0.19rem;
+                        }
+                        .navigation_mobile_second_menu{
+                            background: #030566;
+                            margin-top: 0.2rem;
+                            .navigation_mobile_second_list_item {
+                                padding: 0.15rem 0px;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    @media screen  and (max-width: 420px){
+        .navigation_container{
+            .navigation_content_wrap{
+                padding: 0.2rem;
+                .navigation_left_content{
+                    .navigation_left_list_content{
+                        display: none;
+                    }
+                    .navigation_logo_content{
+                        a{
                             img{
-                                width: 100%;
+                                width: 1.2rem;
+                            }
+                        }
+                    }
+                }
+                .navigation_right_content{
+
+                    .navigation_right_lang_content{
+                        margin-left: 0.2rem;
+                        width: 1rem;
+                        .navigation_lang_content{
+                            span{
+                                padding-left: 0.15rem;
+                                padding-right: 0;
+                            }
+                        }
+                    }
+                    .navigation_right_link_content{
+                        display: none;
+                    }
+                    .mobile_right_content{
+                        display: block;
+                        width: 0.2rem;
+                        img{
+                            width: 100%;
+                        }
+                    }
+                }
+            }
+            .navigation_mobile_content{
+                display: block;
+                padding-bottom: 0.02rem;
+                .navigation_mobile_list_content{
+                    text-align: center;
+                    .navigation_mobile_list{
+                        padding: 0.1rem 0;
+                        a{
+                            color: #fff;
+                        }
+                        .navigation_mobile_item{
+                            font-size: 0.16rem;
+                            font-weight:400;
+                            color:rgba(255,255,255,1);
+                            line-height: 0.19rem;
+                        }
+                        .navigation_mobile_second_menu{
+                            background: #030566;
+                            margin-top: 0.2rem;
+                            .navigation_mobile_second_list_item {
+                                padding: 0.15rem 0;
                             }
                         }
                     }
