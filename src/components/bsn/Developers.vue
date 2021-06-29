60cfab0c7327611acb21985ad62c3121dab82a4a
jinbiao <zjb742626083>
2021-06-29 19:29:11 +0800

    update developer page

--- src/components/bsn/Developers.vue
+++ src/components/bsn/Developers.vue
@@ -36,15 +36,15 @@
 		<div class="testnet_container">
 			<h3 class="testnet_title">{{$store.state.messages.developer.testnet.title}}</h3>
 			<div class="testnet_item_container">
-				<div class="testnet_item" v-for="(item,index) in $store.state.messages.developer.testnet.list" :key="index">
-					<div>
-						<div class="testnet_item_title">{{item.title}}</div>
-						<p class="testnet_item_content">{{item.content}}</p>
+				<div class="testnet_card">
+					<div class="testnet_content_wrap">
+						<h3 class="testnet_content_title">{{$store.state.messages.developer.testnet.content.title}}</h3>
+						<p class="testnet_content">{{$store.state.messages.developer.testnet.content.content}}</p>
+						<a class="testnet_content_btn" rel="noreferrer noopener" :href="$store.state.messages.developer.testnet.content.link">{{$store.state.messages.developer.testnet.content.btnLabel}}</a>
 					</div>
-					<a class="testnet_item_link" :href="item.link" rel="noreferrer noopener">
-						{{item.btnLabel}}
-					</a>
-					<router-link v-if="item.isRouterLink" class="testnet_item_link" :to="item.link">{{item.btnLabel}}</router-link>
+					<div class="bg_content">
+<!--						<img src="../../assets/testnet_image.png" alt="">-->
+					</div>
 				</div>
 			</div>
 		</div>
@@ -198,60 +198,59 @@
 				margin-bottom: 1.2rem;
 				display: flex;
 				justify-content: space-between;
-				@media(max-width: 1200px){
-					flex-direction: column;
-					margin-left: 0.2rem;
-					margin-right: 0.2rem;
-				}
-				@media(max-width: 768px){
-					margin-bottom: 0.6rem;
-				}
-				.testnet_item{
-					flex: 1;
-					box-sizing: border-box;
-					padding: 0.43rem 0.6rem;
-					box-shadow: 0 0.12rem 0.5rem 0.06rem rgba(26, 24, 44, 0.09);
+				.testnet_card{
+					width: 100%;
+					box-shadow: 0 0.04rem 0.16rem 0 rgba(26, 24, 44, 0.09);
+					
+					border-radius: 0.08rem;
 					display: flex;
-					flex-direction: column;
 					justify-content: space-between;
-					border-radius: 0.12rem;
-					@media(max-width: 768px){
-						padding-left: 0.33rem;
-						padding-right: 0.33rem;
-					}
-					.testnet_item_title{
-						font-size: 0.32rem;
-						color: rgba(50, 60, 90, 1);
-						line-height: 0.45rem;
-						font-weight: 600;
-					}
-					.testnet_item_content{
-						margin-top: 0.23rem;
-						font-size: 0.22rem;
-						line-height: 0.36rem;
-						color: rgba(155, 165, 190, 1);
-					}
-					.testnet_item_link{
-						margin-top: 0.32rem;
-						width: 2rem;
-						text-align: center;
-						display: inline-block;
-						background: linear-gradient(95deg, #714BE3 0%, #653FD8 100%);
-						box-shadow: 0 0 0.1rem 0 rgba(152, 0, 255, 0.28);
-						font-size: 0.16rem;
-						font-weight: 600;
-						line-height: 0.22rem;
-						padding: 0.14rem ;
-						box-sizing: border-box;
-						color: #fff;
-						border-radius: 0.33rem;
-					}
-				}
-				.testnet_item:first-child{
-					margin-right: 0.6rem;
 					@media(max-width: 1200px){
-						margin-right: 0;
-						margin-bottom: 0.48rem;
+						margin: 0 0.2rem;
+					}
+					.testnet_content_wrap{
+						flex: 1;
+						box-sizing: border-box;
+						padding: 0.36rem 0.32rem;
+						@media(max-width: 460px){
+							padding: 0.24rem 0.16rem;
+						}
+						.testnet_content_title{
+							font-size: 0.24rem;
+							color: rgba(50, 60, 90, 1);
+							line-height: 0.24rem;
+							margin-bottom: 0.24rem;
+						}
+						.testnet_content{
+							font-size: 0.14rem;
+							color: rgba(155, 165, 190, 1);
+							line-height: 0.24rem;
+							max-width: 5.6rem;
+							margin-bottom: 0.24rem;
+						}
+						.testnet_content_btn{
+							display: inline-block;
+							width: 2rem;
+							height: 0.5rem;
+							border-radius: 0.25rem;
+							background: linear-gradient(95deg, #714BE3 0%, #653FD8 100%);
+							color: rgba(255, 255, 255, 1);
+							line-height: 0.5rem;
+							text-align: center;
+						}
+					}
+					.bg_content{
+						flex: 1;
+						width: 100%;
+						height: 100%;
+						background: url("../../assets/testnet_image.png") no-repeat center center;
+						background-size: cover;
+						@media(max-width: 768px){
+							display: none;
+						}
+						img{
+							width: 100%;
+						}
 					}
 				}
 			}
