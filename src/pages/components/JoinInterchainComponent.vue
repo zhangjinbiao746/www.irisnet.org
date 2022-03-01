86c94f7ccf1c1cd01846bd66dc63389b9ac4489b
pxhgit64 <624834917@qq.com>
2022-03-01 10:21:46 +0800

    fix some questions

--- src/pages/components/JoinInterchainComponent.vue
+++ src/pages/components/JoinInterchainComponent.vue
@@ -3,18 +3,24 @@
         <div class="join_chain_wrap">
             <NavigationLine />
             <h4 class="join_chain_title">{{$store.state.messages.home.joinchain.title}}</h4>
-            <div class="big_container">
-                <p class="join_chain_content">{{$store.state.messages.home.joinchain.content}}</p>
+            <div class="deng">
+                <div class="big_container">
+                    <p class="join_chain_content">{{$store.state.messages.home.joinchain.content}}</p>
 
-            </div>
-            <div class="all">
-                <div class="join_chain_btn_content"
-                    v-for="(item,index) in $store.state.messages.home.joinchain.list"
-                    :key="index">
-                    <div class="chain_btn"><a :href="item.url"
+                </div>
+                <div class="all">
+                    <div class="join_chain_btn_content"
+                        v-for="(item,index) in $store.state.messages.home.joinchain.list"
+                        :key="index">
+                        <!-- <div class="chain_btn"><a :href="item.url"
                             target="_blank"
-                            rel="noreferrer noopener">{{item.btn}}</a></div>
+                            rel="noreferrer noopener">{{item.btn}}</a></div> -->
+                        <a :href="item.url"
+                            class="join_btn"
+                            target="_blank"
+                            rel="noreferrer noopener">{{item.btn}}</a>
 
+                    </div>
                 </div>
             </div>
 
@@ -34,7 +40,7 @@ export default {
 <style scoped lang="less">
 .join_chain_container {
     background: rgba(13, 14, 44, 1);
-    height: 6.67rem;
+    // height: 6.67rem;
 
     padding-bottom: 1rem;
 
@@ -53,18 +59,19 @@ export default {
             height: auto;
             background-position: bottom center !important;
         }
-        @media (max-width: 360px) {
-            height: auto;
-            background-position: bottom center !important;
-            background-size: 100% auto !important;
-        }
-        @media (max-width: 1200px) {
-            margin: 0 0.48rem;
-        }
-        @media (max-width: 596px) {
-            margin: 0 0.2rem;
-        }
+        // @media (max-width: 360px) {
+        //     height: auto;
+        //     background-position: bottom center !important;
+        //     background-size: 100% auto !important;
+        // }
+        // @media (max-width: 1200px) {
+        //     margin: 0 0.48rem;
+        // }
+        // @media (max-width: 596px) {
+        //     margin: 0 0.2rem;
+        // }
         .join_chain_title {
+            font-family: ArialMT;
             padding-top: 0.36rem;
             font-weight: normal;
             font-size: 0.4rem;
@@ -77,51 +84,68 @@ export default {
         .big_container {
             margin-top: 3.75rem;
             display: flex;
-            flex-direction: column;
+            // flex-direction: column;
             justify-content: center;
-            align-items: center;
+
+            padding: 0 0.32rem;
             .join_chain_content {
+                align-self: center;
                 margin: 0 auto;
                 height: 0.32rem;
                 font-size: 0.2rem;
                 color: #ffffff;
                 line-height: 0.32rem;
+                font-family: ArialMT;
+                text-align: center;
             }
         }
         .all {
             display: flex;
             justify-content: center;
             margin-top: 0.6rem;
-            @media (max-width: 400px) {
+            padding-bottom: 0.6rem;
+            @media (max-width: 565px) {
                 flex-direction: column;
+
+                .join_chain_btn_content {
+                    margin-top: 0.32rem;
+                    align-self: center;
+                }
             }
 
             .join_chain_btn_content {
                 box-sizing: border-box;
-                min-width: 2rem;
-                display: inline-block;
+                width: 2.4rem;
+                height: 0.56rem;
+                // display: inline-block;
                 background: rgba(46, 69, 211, 1);
                 color: rgba(255, 255, 255, 1);
-                padding: 0.14rem 0.08rem;
+                // padding: 0.14rem 0.08rem;
                 text-align: center;
                 border-radius: 0.04rem;
+                font-family: ArialMT;
                 font-size: 0.2rem;
-                margin-right: 0.32rem;
+                margin-right: 0.16rem;
+                margin-left: 0.16rem;
 
-                @media (max-width: 1100px) {
-                    justify-content: center;
-                }
-                @media (max-width: 768px) {
-                    margin-top: 0.32rem;
-                }
+                // @media (max-width: 1100px) {
+                //     justify-content: center;
+                // }
+                // @media (max-width: 768px) {
+                //     margin-top: 0.32rem;
+                // }
 
-                .chain_btn a {
-                    margin: 0 auto;
+                .join_btn {
+                    display: block;
+                    height: 0.56rem;
+                    width: 100%;
+                    padding: 0.14rem 0.08rem;
 
                     box-sizing: border-box;
-                    line-height: 0.28rem;
+
                     font-size: 0.2rem;
                     border-radius: 0.04rem;
+                    font-family: ArialMT;
 
                     font-weight: 600;
                     color: #ffffff;
