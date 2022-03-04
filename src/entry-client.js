6d59d3cd26d4c774e4e5b85891989b602783ea3b
jinbiao <zjb742626083>
2022-03-04 11:45:42 +0800

    update roadmap component

--- src/entry-client.js
+++ src/entry-client.js
@@ -5,11 +5,4 @@ const { app, store } = createApp()
 if (window.__INITIAL_STATE__) {
   store.replaceState(window.__INITIAL_STATE__)
 }
-console.log(app)
-app.errorHandler = (err, instance, info) => {
-    // report error to tracking services
-    console.log(err,'err')
-    console.log(instance,'instance')
-    console.log(info,'info')
-}
 app.$mount('#app')
