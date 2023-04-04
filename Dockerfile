56cfeca36b5376d3229af8df34f4c6a12e7a66ce
shanshan <shan@bianjie.ai>
2023-04-04 18:59:02 +0800

    refactor

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM keymetrics/pm2:latest-alpine
-
-ADD . /www.irisnet.org
-WORKDIR /www.irisnet.org
-RUN  yarn install && yarn run build
-EXPOSE 3000
-ENV NODE_ENV="production"
-CMD ["pm2-runtime", "server.js"]
--- src/umeng/umeng.js
+++
@@ -1,9 +0,0 @@
-export default {
-	push(category,action,label,value,nodeid) {
-		if(window._czc){
-			window._czc.push(["_trackEvent",category,action,label,value,nodeid])
-		}else {
-			console.error('error')
-		}
-	}
-}
